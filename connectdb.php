2b4cb07221ceb13c04d9e00ef40078cbc548d475
MATan1808 <105353487+MATan1808@users.noreply.github.com>
2024-03-24 21:23:53 +0700

    Add files via upload
    ket noi SQL
    
    Signed-off-by: MATan1808 <105353487+MATan1808@users.noreply.github.com>

--- connectdb.php
+++ connectdb.php
@@ -3,14 +3,13 @@ function connectdb(){
 $servername = "localhost";
 $username = "root";
 $password = "";
-
 try {
     $conn = new PDO("mysql:host=$servername;dbname=web_giapha", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
+    echo "Connected successfully";
   } catch(PDOException $e) {
-    // echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
   }
   return $conn;
 }
